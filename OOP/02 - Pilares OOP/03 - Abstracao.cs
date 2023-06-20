a6685aa71389d47e86671f0b76c573330f132556
felipe <felipe.cardoso@go-it.work>
2023-06-19 21:13:44 -0300

    first

---
+++ OOP/02 - Pilares OOP/03 - Abstracao.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP
+{          //Função do abstract
+    public abstract class Eletrodomestico // Essa classe só pode ser herdada e nunca estanciada!
+    {
+        private readonly string _nome;
+        private readonly int _voltagem;
+        protected Eletrodomestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        public abstract void Ligar(); // Não sou obrigado a implementar esse método nessa clase pois ele é abstrato mas quem herdar precisa
+        public abstract void Desligar();
+
+        public virtual void Testar() // Sou obrigado a implementar o metodo aqui mas quem herdar nao precisa usar esse metodo
+        {
+            //testar
+        }
+    }
+}
---
+++ OOP/02 - Pilares OOP/04 - Polimorfismo.cs
@@ -0,0 +1,40 @@
+﻿namespace OOP
+{
+    // Poli-morfismo
+    public sealed class CafeteiraEspressa : Eletrodomestico
+    {
+        public CafeteiraEspressa(string nome, int voltagem)
+            : base(nome, voltagem) { }
+
+        public CafeteiraEspressa()
+            : base("Padrao", 220) {  }
+
+        private static void AquecerAgua() { }
+
+        private static void MoerGraos() { }
+
+        public void PrepararCafe()
+        {
+            Testar();
+            AquecerAgua();
+            MoerGraos();
+            // ETC...
+        }
+
+        public override void Testar()
+        {
+            //testar
+        }
+   
+              //Sobreescrever
+        public override void Ligar()
+        {
+            // Verificar recipiente de agua
+        }
+
+        public override void Desligar()
+        {
+            // Resfriar aquecedor
+        }
+    }
+}
